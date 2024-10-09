18749cf82a7d4dd0945268c299bcb3cc813c7a94
nithin.soundar <nithin.soundar@westernacher.com>
2024-10-09 17:56:48 +0530

    new dependencies

--- user/user/src/main/java/com/hexagonal/user/adapters/rest/UserFileController.java
+++ user/user/src/main/java/com/hexagonal/user/adapters/rest/UserFileController.java
@@ -6,13 +6,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
