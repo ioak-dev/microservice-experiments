a892a30d878bc8afcf4eccdd1297b979fcfe894c
nithin.soundar <nithin.soundar@westernacher.com>
2024-12-20 10:58:50 +0530

    microsoft auth

---
+++ security-microsoftAuth/src/main/java/com/example/securitymicrosoftauth/SecurityMicrosoftAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitymicrosoftauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityMicrosoftAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityMicrosoftAuthApplication.class, args);
+	}
+
+}
---
+++ security-microsoftAuth/src/test/java/com/example/securitymicrosoftauth/SecurityMicrosoftAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitymicrosoftauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMicrosoftAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
